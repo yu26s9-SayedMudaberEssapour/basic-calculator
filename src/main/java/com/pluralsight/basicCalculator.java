d72b2a670722199de73b0fc9048402874e4bf0d6
sayed essapour <sessapour@my.yearupunited.org>
2026-04-10 17:01:19 -0400

    Made sure to write two methods to get the first and second numbers

--- src/main/java/com/pluralsight/basicCalculator.java
+++ src/main/java/com/pluralsight/basicCalculator.java
@@ -7,16 +7,12 @@ public class basicCalculator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        //asking for the first number
-        System.out.print("Enter your first Number: ");
-        int firstNum = scanner.nextInt();
-
-
+        //this should get the first number
+        int firstNumber = getFirstNum(scanner);
 
         //asking for the second number
-        System.out.print("Enter your Second Number: ");
-        int secondNum = scanner.nextInt();
-        String a = scanner.nextLine();
+        int seconfNumber = getSecondNum(scanner);
+
 
         System.out.println("Possible calculations: \n (A)add \n (S)subtract \n (M)multiply \n (D)divide");
 
@@ -26,29 +22,46 @@ public class basicCalculator {
         String sign = "";
 
         if (letter.equalsIgnoreCase("A")) {
-            answer = firstNum + secondNum;
+            answer = firstNumber + seconfNumber;
             sign = "+";
 
         } else if (letter.equalsIgnoreCase("S")) {
-            answer = firstNum - secondNum;
+            answer = firstNumber - seconfNumber;
             sign = "-";
 
         } else if (letter.equalsIgnoreCase("M")) {
-            answer = firstNum * secondNum;
+            answer = firstNumber * seconfNumber;
             sign = "*";
 
         } else if (letter.equalsIgnoreCase("D")) {
-            answer = firstNum / secondNum;
+            answer = firstNumber / seconfNumber;
             sign = "/";
         } else {
             System.out.println("Not one of the options, Sorry!");
 
         }
 
-        System.out.printf("your answer is: %d %s %d = %f" , firstNum, sign, secondNum,answer);
+        System.out.printf("your answer is: %d %s %d = %f", firstNumber, sign, seconfNumber, answer);
 
 
     }
+
+    public static int getFirstNum(Scanner scanner) {
+        //asking for the first number
+        System.out.print("Enter your first Number: ");
+        int firstNum = scanner.nextInt();
+        return firstNum;
+    }
+
+    public static int getSecondNum(Scanner scanner)
+    {
+        System.out.print("Enter your Second Number: ");
+        int secondNum = scanner.nextInt();
+        String a = scanner.nextLine();
+        return secondNum;
+    }
+
+
 
 }
 
